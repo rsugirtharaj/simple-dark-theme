25e3ce707b94680af7c45edb988c881fae244e1e
GitHub Action <action@github.com>
2021-12-11 07:15:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/rsugirtharaj/simpledarktheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/rsugirtharaj/simpledarktheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rsugirtharaj.simpledarktheme.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.rsugirtharaj.simpledarktheme.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/rsugirtharaj/simpledarktheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rsugirtharaj.simpledarktheme.services
+
+import com.github.rsugirtharaj.simpledarktheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
